886fef116d37bbc8666765a4ab85aedf3f4ea223
Mathuranath <mbharathbabu@paypal.com>
2022-04-03 00:03:08 +0530

    Introduction

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,12 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <h1>Ruhtam</h1>
+      <h3>Welcome aboard!</h3>
+      <p>I am Mathur aka Ruhtam, a devout engineer, with strong experience in building performant distributed system, with services architecture in Java stack. Built clean websites and webapps with HTML, CSS and with new age frameworks React, Next.js and Remix.
+      </p>
+      <p>I have around 9 years of experience in the IT and Finance Industry, worked on some intense and highly available, scalable systems to some offbeat modem programs for SMS Auth, IVR systems with Asterisk and interesting projects. Checkout my career <a target="_blank" href="https://www.linkedin.com/in/mathuranath">here</a>!</p>
+      {/*<p>Reach out to me for any computer science or software engineering work. I offer consultations, end-to-end solutions, turn-key development, trainings and much more. In case you're doubtful, shoot me a mail!!</p>*/}
+    </main>
   );
 }
