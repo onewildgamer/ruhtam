c7e50b75e2cbb13a0d2308208cba94f9fc0b49d4
onewildgamer <mathu92@gmail.com>
2022-04-17 08:33:38 +0530

    Edit experience

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,8 +5,9 @@ export default function Index() {
       <h3>Welcome aboard!</h3>
       <p>I am Mathur aka Ruhtam, a devout engineer, with strong experience in building performant distributed system, with services architecture in Java stack. Built clean websites and webapps with HTML, CSS and with new age frameworks React, Next.js and Remix.
       </p>
-      <p>I have around 9 years of experience in the IT and Finance Industry, worked on some intense and highly available, scalable systems to some offbeat modem programs for SMS Auth, IVR systems with Asterisk and interesting projects. Checkout my career <a target="_blank" href="https://www.linkedin.com/in/mathuranath">here</a>!</p>
+      <p>I have around 9 years of experience in building software systems, have worked on some intense and highly available, scalable systems to offbeat GSM modem programs, IVR systems with Asterisk. Checkout my career <a target="_blank" href="https://www.linkedin.com/in/mathuranath">here</a>!</p>
       {/*<p>Reach out to me for any computer science or software engineering work. I offer consultations, end-to-end solutions, turn-key development, trainings and much more. In case you're doubtful, shoot me a mail!!</p>*/}
     </main>
   );
 }
+
